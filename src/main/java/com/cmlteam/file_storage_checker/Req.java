ee4cbfa6a09f03aedeb077cb3b2276fd71cffae9
xonix <xonixx@gmail.com>
2020-10-15 18:13:47 +0300

    req, resp

--- src/main/java/com/cmlteam/file_storage_checker/Req.java
+++ src/main/java/com/cmlteam/file_storage_checker/Req.java
@@ -2,15 +2,25 @@ package com.cmlteam.file_storage_checker;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 @RequiredArgsConstructor
 public class Req {
 
-    private final RestTemplate restTemplate;
+  private final RestTemplate restTemplate;
 
-    Resp get(String url, Object... uriVariables) {
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class, uriVariables);
-        return new Resp(responseEntity);
+  Resp get(String url, Object... uriVariables) {
+    ResponseEntity<String> responseEntity;
+    try {
+
+      responseEntity = restTemplate.getForEntity(url, String.class, uriVariables);
+    } catch (RestClientResponseException e) {
+      responseEntity =
+          ResponseEntity.status(e.getRawStatusCode())
+              .headers(e.getResponseHeaders())
+              .body(e.getResponseBodyAsString());
     }
+    return new Resp(responseEntity);
+  }
 }
