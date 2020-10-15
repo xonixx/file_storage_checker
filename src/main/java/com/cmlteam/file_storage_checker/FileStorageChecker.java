713e579066403df92e206e4f1316b8893df64f65
xonix <xonixx@gmail.com>
2020-10-15 18:40:45 +0300

    req, resp

--- src/main/java/com/cmlteam/file_storage_checker/FileStorageChecker.java
+++ src/main/java/com/cmlteam/file_storage_checker/FileStorageChecker.java
@@ -1,5 +1,6 @@
 package com.cmlteam.file_storage_checker;
 
+import com.cmlteam.file_storage_checker.util.JsonUtil;
 import lombok.AllArgsConstructor;
 
 import javax.annotation.PostConstruct;
@@ -10,7 +11,15 @@ public class FileStorageChecker {
 
   @PostConstruct
   void run() {
-    Resp resp = req.get("http://localhost:8080/file");
+    String ENDPOINT = "http://localhost:8080/file";
+    Resp resp = req.get(ENDPOINT);
     System.out.println(resp);
+
+    Resp resp1 = req.post(ENDPOINT, JsonUtil.json().add("name", "ZZZZ").add("size", 123));
+
+    System.out.println(resp1);
+
+    Resp resp3 = req.get(ENDPOINT);
+    System.out.println(resp3);
   }
 }
--- src/main/java/com/cmlteam/file_storage_checker/Req.java
+++ src/main/java/com/cmlteam/file_storage_checker/Req.java
@@ -1,9 +1,14 @@
 package com.cmlteam.file_storage_checker;
 
+import com.cmlteam.file_storage_checker.util.JsonUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 @RequiredArgsConstructor
 public class Req {
@@ -11,10 +16,30 @@ public class Req {
   private final RestTemplate restTemplate;
 
   Resp get(String url, Object... uriVariables) {
+    return exec(() -> restTemplate.getForEntity(url, String.class, uriVariables));
+  }
+
+  Resp post(String url, JsonUtil.JsonBuilder body, Object... uriVariables) {
+    return exec(
+        () ->
+            restTemplate.postForEntity(
+                url,
+                RequestEntity.post(URI.create(url))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(body.toString()),
+                String.class,
+                uriVariables));
+  }
+
+  @FunctionalInterface
+  private interface Call {
+    ResponseEntity<String> call();
+  }
+
+  private Resp exec(Call call) {
     ResponseEntity<String> responseEntity;
     try {
-
-      responseEntity = restTemplate.getForEntity(url, String.class, uriVariables);
+      responseEntity = call.call();
     } catch (RestClientResponseException e) {
       responseEntity =
           ResponseEntity.status(e.getRawStatusCode())
