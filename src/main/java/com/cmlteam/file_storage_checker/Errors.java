4de0bbf7638d2bfe0013c739d6295e3d5a8cd7c9
xonix <xonixx@gmail.com>
2020-10-16 03:36:43 +0300

    unique errors

--- src/main/java/com/cmlteam/file_storage_checker/Errors.java
+++ src/main/java/com/cmlteam/file_storage_checker/Errors.java
@@ -1,16 +1,22 @@
 package com.cmlteam.file_storage_checker;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Errors {
 
   private final List<String> errorMsgs = new ArrayList<>();
+  private final Set<String> uniqueErrors = new HashSet<>();
   private int i = 0;
 
   void addError(String msg, Resp resp) {
-    String line = "\n------------\n";
-    errorMsgs.add((++i) + ". " + msg + ". Response was: " + line + resp + line);
+    if (!uniqueErrors.contains(msg)) {
+      uniqueErrors.add(msg);
+      String line = "\n------------\n";
+      errorMsgs.add((++i) + ". " + msg + ". Response was: " + line + resp + line);
+    }
   }
 
   public String report() {
